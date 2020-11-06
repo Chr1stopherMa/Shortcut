12f3e6ab0411b54c5f3e2c7cf53885cfae807bb6
Christopher Ma <chrisma200@gmail.com>
2020-11-06 18:07:16 -0500

    Open app based on name instead of package

--- app/src/main/java/com/familyservicestoronto/shortcut/MainActivity.java
+++ app/src/main/java/com/familyservicestoronto/shortcut/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // test opening Facebook
+        try {
+            ActivitySwitchUtil.openApp(getApplicationContext(), ExternalApp.FACEBOOK);
+        } catch (AppNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
