a56e7c2f93f115f4e80e7686e5af234b92faf16a
Rabia'h Malik <rmalik999@hotmail.com>
2020-11-15 16:19:35 -0500

    Added page for facebook tutorials

---
+++ app/src/main/java/com/familyservicestoronto/shortcut/FacebookTutorial.java
@@ -0,0 +1,14 @@
+package com.familyservicestoronto.shortcut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FacebookTutorial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_tutorial);
+    }
+}
