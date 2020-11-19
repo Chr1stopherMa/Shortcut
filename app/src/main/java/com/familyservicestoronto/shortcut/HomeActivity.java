75e9e8fd4a0a9750350a6ce9f5af45f3ce6c2c7a
wongbasi <Wongchifung99!>
2020-11-18 22:33:42 -0500

    link the whatsapp tut to the icon, all functions and features for whatsapp have been implemented

--- app/src/main/java/com/familyservicestoronto/shortcut/HomeActivity.java
+++ app/src/main/java/com/familyservicestoronto/shortcut/HomeActivity.java
@@ -2,13 +2,24 @@ package com.familyservicestoronto.shortcut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
-
+ImageView wtsapp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        wtsapp=findViewById(R.id.WhatsappIcon);
+        wtsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, WhatsappTutActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
