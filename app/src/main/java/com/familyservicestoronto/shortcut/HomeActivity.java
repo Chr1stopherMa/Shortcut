29a8f115d5dbba183be3c33366af6bbe4b1b260d
ashasmemon <ashas.memon@mail.utoronto.ca>
2020-11-18 22:58:16 -0500

    Linked google and whatsapp tutorials from HomeActivity screen

--- app/src/main/java/com/familyservicestoronto/shortcut/HomeActivity.java
+++ app/src/main/java/com/familyservicestoronto/shortcut/HomeActivity.java
@@ -5,21 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
-ImageView wtsapp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        wtsapp=findViewById(R.id.WhatsappIcon);
-        wtsapp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(HomeActivity.this, WhatsappTutActivity.class);
-                startActivity(intent);
-            }
-        });
+
+        ImageView whatsapp = findViewById(R.id.WhatsappIcon);
+        whatsapp.setOnClickListener(HomeActivity.this::loadWhatsappTutorialActivity);
+
+        ImageView goToGoogle = findViewById(R.id.GoogleIcon);
+        goToGoogle.setOnClickListener(HomeActivity.this::loadGoogleSearchActivity);
+    }
+
+    public void loadWhatsappTutorialActivity(View view) {
+        Intent intent = new Intent(HomeActivity.this, WhatsappTutActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadGoogleSearchActivity(View view) {
+        Intent intent = new Intent(HomeActivity.this, GoogleSearchActivity.class);
+        startActivity(intent);
     }
 }
